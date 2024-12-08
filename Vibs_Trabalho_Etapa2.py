ff6a25dbf482d103039a5dc61fec917c64d08f7a
Ian M Meirelles <ianmmeirelles@outlook.com>
2024-12-08 12:02:06 -0300

    update: etapas 1 e 2

--- Vibs_Trabalho_Etapa2.py
+++ Vibs_Trabalho_Etapa2.py
@@ -18,7 +18,6 @@ import pandas as pd
 from scipy.optimize import fsolve
 from scipy.signal import find_peaks
 from scipy.optimize import minimize
-from matplotlib.patches import Circle
 ## ============================================================================
 ## RC parameters
 ## ============================================================================
@@ -34,7 +33,7 @@ font = font_manager.FontProperties(family='serif',
 
 
 #Dataframes
-df1 = pd.read_csv('Grupo - 3 9, Signal 2.csv', header=17, sep=";", decimal=',')
+df1 = pd.read_csv('Medição Livre 6, Signal 2.csv', header=17, sep=";", decimal=',')
 df1.rename(columns = {"'": 'Frequency', '(m/s^2)/N': 'FRF_real', '(m/s^2)/N.1': 'FRF_imag'}, 
            inplace = True)
 df1 = df1.apply(pd.to_numeric, errors='coerce')
@@ -44,7 +43,7 @@ df1['Frequency'] = df1['Frequency']*2*np.pi
 df1["FRF_pos"] = df1['FRF_abs']/(df1['Frequency']**2)
 
 
-df2 = pd.read_csv('Grupo - 3 9, Signal 3.csv', header=17, sep=";", decimal=',')
+df2 = pd.read_csv('Medição Livre 6, Signal 3.csv', header=17, sep=";", decimal=',')
 df2.rename(columns = {"'": 'Frequency', '(m/s^2)/N': 'FRF_real', '(m/s^2)/N.1': 'FRF_imag'}, 
            inplace = True)
 df2 = df2.apply(pd.to_numeric, errors='coerce')
@@ -53,7 +52,8 @@ df2['Frequency'][0] = 1
 df2['Frequency'] = df2['Frequency']*2*np.pi
 df2["FRF_pos"] = df2['FRF_abs']/(df2['Frequency']**2)
 
-df3 = pd.read_csv('Grupo - 3 9, Signal 4.csv', header=17, sep=";", decimal=',')
+
+df3 = pd.read_csv('Medição Livre 6, Signal 4.csv', header=17, sep=";", decimal=',')
 df3.rename(columns = {"'": 'Frequency', '(m/s^2)/N': 'FRF_real', '(m/s^2)/N.1': 'FRF_imag'}, 
            inplace = True)
 df3 = df3.apply(pd.to_numeric, errors='coerce')
@@ -62,10 +62,11 @@ df3['Frequency'][0] = 1
 df3['Frequency'] = df3['Frequency']*2*np.pi
 df3["FRF_pos"] = df3['FRF_abs']/(df3['Frequency']**2)
 
+
 #Identificação dos Picos
 peaks1, _ = find_peaks(df1['FRF_pos'], height=1e-6)
 peaks2, _ = find_peaks(df2['FRF_pos'], height=1e-6)
-peaks3, _ = find_peaks(df3['FRF_pos'], height=1e-6)
+peaks3, _ = find_peaks(df3['FRF_pos'], height=2e-6)
 
 #Funções 
 def calculate_ssi(observed, calculated):
@@ -86,22 +87,22 @@ def meia_potencia(df, peaks):
             if df['FRF_pos'].iloc[i] < meia_pot:
                 w1 = df['Frequency'].iloc[i]
                 break
-            
+       
         w2 = None
         for i in range(peak_idx + 1, len(df['FRF_pos'])):
             if df['FRF_pos'].iloc[i] < meia_pot:
                 w2 = df['Frequency'].iloc[i]
                 break
-            
+        
         if w1 is not None and w2 is not None:
             bandwidths.append((w1,w2))
         else:
-            bandwidths.append((None, None))
-            
+            bandwidths.append((df1['Frequency'][22], df1['Frequency'][28]))    
     return bandwidths
 
+
 # def func_erro(qsi, df_1, df_2, df_3, wi, V, w):
-#     for j in range(len(w)):
+#     for j in range(1, len(w)):
 #         soma1 = 0
 #         soma2 = 0
 #         soma3 = 0
@@ -115,9 +116,9 @@ def meia_potencia(df, peaks):
 #             term3 = V[x3, k] * V[xf3, k] / (wi[k]**2 - w[j]**2 + 1j * 2 * qsi[k] * wi[k] * w[j])
 #             soma3 += term3
                         
-#         H12_ot[j] = soma1
-#         H22_ot[j] = soma2
-#         H32_ot[j] = soma3
+#         H12_ot[j] = soma1 * w[j]**2
+#         H22_ot[j] = soma2 * w[j]**2
+#         H32_ot[j] = soma3 * w[j]**2
         
 #     FRF_12_ot = np.abs(H12_ot)
 #     FRF_22_ot = np.abs(H22_ot)
@@ -126,9 +127,9 @@ def meia_potencia(df, peaks):
 #     func_erro.FRF_22_ot = FRF_22_ot
 #     func_erro.FRF_32_ot = FRF_32_ot
 
-#     ssi1_ot = calculate_ssi(df1['FRF_pos'][2:], FRF_12_ot[2:])
-#     ssi2_ot = calculate_ssi(df2['FRF_pos'][2:], FRF_22_ot[2:])
-#     ssi3_ot = calculate_ssi(df3['FRF_pos'][2:], FRF_32_ot[2:])
+#     ssi1_ot = calculate_ssi(df1['FRF_abs'], FRF_12_ot)
+#     ssi2_ot = calculate_ssi(df2['FRF_abs'], FRF_22_ot)
+#     ssi3_ot = calculate_ssi(df3['FRF_abs'], FRF_32_ot)
     
 #     return -(ssi1_ot+ssi2_ot+ssi3_ot)/3
 
@@ -165,16 +166,13 @@ massa_vigas = 4 * (48/35) * volume_viga * densidade_al # kg
 
 
 # Graus de Liberdade
-N = 3
+N = 4
 
 # Massas
-m_e1 = 646e-3 # kg
-m_e2 = 1187e-3 # kg
-m_e3 = 1635e-3 # kg
-
-m_1 = massa_piso + m_e1 + massa_vigas
-m_2 = massa_piso + m_e2 + massa_vigas
-m_3 = massa_piso + m_e3 + massa_vigas
+m_b = massa_piso+ massa_vigas
+m_1 = massa_piso+ massa_vigas
+m_2 = massa_piso + massa_vigas
+m_3 = massa_piso + massa_vigas
 
 
 # Rigidez
@@ -184,11 +182,12 @@ k3 = k_eq
 
 
 # Matriz de Rigidez e de Massa
-K = np.array([[k1 + k2,     -k2,   0],
-              [    -k2, k2 + k3, -k3],
-              [      0,     -k3, k3]])
+K = np.array([[ k1,     -k1,       0,   0],
+              [-k1, k1 + k2,     -k2,   0],
+              [  0,     -k2, k2 + k3, -k3],
+              [  0,       0,     -k3,  k3]])
 
-Mv = np.array([m_1, m_2, m_3])
+Mv = np.array([m_b, m_1, m_2, m_3])
 M = np.diag(Mv)
 
 
@@ -203,29 +202,31 @@ for i in range(V.shape[1]):
     norm_factor = np.sqrt(V[:, i].T @ M @ V[:, i])
     V[:, i] /= norm_factor
    
-    
+W[0] = 0
+
+
 # Frequências Naturais
 wi = np.sqrt(W)
 fi = wi / (2 * np.pi)
 
 
-# Frequências Analisadas
+# # Frequências Analisadas
 f = np.arange(0, 201, 1) 
 w = 2*np.pi*f  # Angular frequency
 
 
-#Criação de Vetores
+# #Criação de Vetores
 H12 = np.zeros_like(w, dtype=complex)
-x1 = 0  
-xf1 = 1
+x1 = 1  
+xf1 = 2
 
 H22 = np.zeros_like(w, dtype=complex)
-x2 = 1
-xf2 = 1
+x2 = 2
+xf2 = 2
 
 H32 = np.zeros_like(w, dtype=complex)
-x3 = 2
-xf3 = 1
+x3 = 3
+xf3 = 2
 
 Hm1 = np.zeros((len(w), N), dtype=complex)
 Hm2 = np.zeros((len(w), N), dtype=complex)
@@ -239,15 +240,14 @@ qsi_banda = np.zeros(N)
 
 
 #Cálculo dos qsi
-for k in range(N):
-    bands = meia_potencia(df3, peaks3)[k]
-    qsi_banda[k] = (bands[1] - bands[0])/(2*df3['Frequency'][peaks3[k]])
+for k in range(len(peaks1)):
+    bands = meia_potencia(df1, peaks1)[k]
+    qsi_banda[k+1] = (bands[1] - bands[0])/(2*df1['Frequency'][peaks1[k]])
 
 qsi = qsi_banda
 
-
 #Cálculos das FRFs
-for j in range(5, len(w)):
+for j in range(5,len(w)):
     soma1 = 0
     soma2 = 0
     soma3 = 0
@@ -269,24 +269,25 @@ for j in range(5, len(w)):
     H22[j] = soma2
     H32[j] = soma3
 
+
 Hm12 = np.abs(Hm1)
 Hm22 = np.abs(Hm2)
 Hm32 = np.abs(Hm3)
-    
+
 FRF_12 = np.abs(H12)
 FRF_22 = np.abs(H22)
 FRF_32 = np.abs(H32)
 
 
-#Cálculo das similaridades
-ssi1 = calculate_ssi(df1['FRF_pos'], FRF_12)
-ssi2 = calculate_ssi(df2['FRF_pos'], FRF_22)
-ssi3 = calculate_ssi(df3['FRF_pos'], FRF_32)
-print((ssi1+ssi2+ssi3)/3)
+# #Cálculo das similaridades
+# ssi1 = calculate_ssi(df1['FRF_pos'], FRF_12)
+# ssi2 = calculate_ssi(df2['FRF_pos'], FRF_22)
+# ssi3 = calculate_ssi(df3['FRF_pos'], FRF_32)
+# print((ssi1+ssi2+ssi3)/3)
 
 
 # #Otimização
-# res = minimize(func_erro, qsi_banda, args=(df1, df2, df3, wi, V, w), method='L-BFGS-B', bounds=[(0, 1)] * N)
+# res = minimize(func_erro, qsi_banda, args=(df1, df2, df3, wi, V, w), method='L-BFGS-B', bounds=[(0, 0.99)] * N)
 # print(res.fun*-1)
 # qsi_otimizado = res.x
 # FRF_12_otimizado = func_erro.FRF_12_ot
@@ -294,19 +295,28 @@ print((ssi1+ssi2+ssi3)/3)
 # FRF_32_otimizado = func_erro.FRF_32_ot
 
 
-#Autevetores
-GL = np.zeros(N)
-for i in range(N):
-    GL[i] = i
+FRF_32_rms = np.zeros_like(FRF_32)
+FRF_32_rms_exp = np.zeros_like(FRF_32)
+FRF_sum = 0
+FRF_sum_exp = 0
+F0 = 1 #Força unitária do delta de Dirac
+
+for k in range(5, len(w)):
+    FRF_32_rms[k] = FRF_32[k] ** 2
+    FRF_sum += FRF_32_rms[k]*2*np.pi #Passo delta_w é 2*pi
+    FRF_32_rms_exp[k] = df3['FRF_pos'][k]**2
+    FRF_sum_exp += FRF_32_rms_exp[k]*2*np.pi
     
-V1 = [V[0][0], V[1][0], V[2][0]]
-V2 = [V[0][1], V[1][1], V[2][1]]
-V3 = [V[0][2], V[1][2], V[2][2]]
+    
+    
+x_rms = F0 * np.sqrt(FRF_sum)
+x_rms_exp = F0 * np.sqrt(FRF_sum_exp)
+print(x_rms, x_rms_exp)
 
 
-# # Plot da FRF_12
+# Plot da FRF_12
 plt.figure(1)
-# plt.semilogy(w[2:], FRF_12_otimizado[2:], '--r', linewidth=2, label = 'Otimizado')
+#plt.semilogy(w, FRF_12_otimizado, '--r', linewidth=2, label = 'Otimizado')
 plt.semilogy(w[5:], FRF_12[5:], '--k', linewidth=2, label= 'Modelo')
 plt.semilogy(df1['Frequency'][5:], df1['FRF_pos'][5:], '--b', linewidth=2, label = 'Experimental')
 plt.grid()
@@ -316,9 +326,9 @@ plt.legend()
 plt.show()
 
 
-# # Plot da FRF_22
+# Plot da FRF_22
 plt.figure(2)
-# plt.semilogy(w[2:], FRF_22_otimizado[2:], '--r', linewidth=2, label = 'Otimizado')
+# #plt.semilogy(w, FRF_22_otimizado, '--r', linewidth=2, label = 'Otimizado')
 plt.semilogy(w[5:], FRF_22[5:], '--k', linewidth=2, label= 'Modelo')
 plt.semilogy(df2['Frequency'][5:], df2['FRF_pos'][5:], '--b', linewidth=2, label = 'Experimental')
 plt.grid()
@@ -328,9 +338,9 @@ plt.legend()
 plt.show()
 
 
-# # Plot da FRF_32
+# Plot da FRF_32
 plt.figure(3)
-# plt.semilogy(w[2:], FRF_32_otimizado[2:], '--r', linewidth=2, label = 'Otimizado')
+#plt.semilogy(w, FRF_32_otimizado, '--r', linewidth=2, label = 'Otimizado')
 plt.semilogy(w[5:], FRF_32[5:], '--k', linewidth=2, label= 'Modelo')
 plt.semilogy(df3['Frequency'][5:], df3['FRF_pos'][5:], '--b', linewidth=2, label = 'Experimental')
 plt.grid()
@@ -342,86 +352,40 @@ plt.show()
 
 # # Plot dos Modos na FRF12
 # plt.figure(4)
-# plt.semilogy(w, Hm12[:, 0], '--g', linewidth=2, label='Modo 1')
-# plt.semilogy(w, Hm12[:, 1], '--r', linewidth=2, label='Modo 2')
-# plt.semilogy(w, Hm12[:, 2], '--b', linewidth=2, label='Modo 3')
+# plt.semilogy(w, Hm12[:, 1], '--g', linewidth=2, label='Mode 1')
+# plt.semilogy(w, Hm12[:, 2], '--r', linewidth=2, label='Mode 2')
+# plt.semilogy(w, Hm12[:, 3], '--b', linewidth=2, label='Mode 3')
 # plt.semilogy(w, FRF_12, 'k', linewidth=2, label='Total')
 # plt.grid()
-# plt.xlabel(r'$\omega$ [rad/s]', fontsize=20)
-# plt.ylabel(r'$|H_{12}(\omega)|$', fontsize=20)
+# plt.xlabel(r'$\omega$ [rad/s]', fontsize=14)
+# plt.ylabel(r'$|H_{ij}(\omega)|$', fontsize=14)
 # plt.legend()
 # plt.show()
 
 
 # # Plot dos Modos na FRF22
 # plt.figure(5)
-# plt.semilogy(w, Hm22[:, 0], '--g', linewidth=2, label='Modo 1')
-# plt.semilogy(w, Hm22[:, 1], '--r', linewidth=2, label='Modo 2')
-# plt.semilogy(w, Hm22[:, 2], '--b', linewidth=2, label='Modo 3')
+# plt.semilogy(w, Hm22[:, 1], '--g', linewidth=2, label='Mode 1')
+# plt.semilogy(w, Hm22[:, 2], '--r', linewidth=2, label='Mode 2')
+# plt.semilogy(w, Hm22[:, 3], '--b', linewidth=2, label='Mode 3')
 # plt.semilogy(w, FRF_22, 'k', linewidth=2, label='Total')
 # plt.grid()
-# plt.xlabel(r'$\omega$ [rad/s]', fontsize=20)
-# plt.ylabel(r'$|H_{22}(\omega)|$', fontsize=20)
+# plt.xlabel(r'$\omega$ [rad/s]', fontsize=14)
+# plt.ylabel(r'$|H_{ij}(\omega)|$', fontsize=14)
 # plt.legend()
 # plt.show()
 
 
 # # Plot dos Modos na FRF32
 # plt.figure(6)
-# plt.semilogy(w, Hm32[:, 0], '--g', linewidth=2, label='Modo 1')
-# plt.semilogy(w, Hm32[:, 1], '--r', linewidth=2, label='Modo 2')
-# plt.semilogy(w, Hm32[:, 2], '--b', linewidth=2, label='Modo 3')
+# plt.semilogy(w, Hm32[:, 1], '--g', linewidth=2, label='Mode 1')
+# plt.semilogy(w, Hm32[:, 2], '--r', linewidth=2, label='Mode 2')
+# plt.semilogy(w, Hm32[:, 3], '--b', linewidth=2, label='Mode 3')
 # plt.semilogy(w, FRF_32, 'k', linewidth=2, label='Total')
 # plt.grid()
-# plt.xlabel(r'$\omega$ [rad/s]', fontsize=20)
-# plt.ylabel(r'$|H_{32}(\omega)|$', fontsize=20)
+# plt.xlabel(r'$\omega$ [rad/s]', fontsize=14)
+# plt.ylabel(r'$|H_{ij}(\omega)|$', fontsize=14)
 # plt.legend()
-# plt.show()
-
-# #Plot Autovetores
-# Circle11 = plt.Circle((GL[0], V1[0]), 0.01, color='k', fill=True)
-# Circle21 = plt.Circle((GL[1], V1[1]), 0.01, color='k', fill=True)
-# Circle31 = plt.Circle((GL[2], V1[2]), 0.01, color='k', fill=True)
-# plt.figure(7)
-# plt.plot(GL, V1, linewidth= 2)
-# plt.grid()
-# plt.xlabel(r'Grau de Liberdade', fontsize= 20)
-# plt.ylabel(r'Auvetor 1', fontsize= 20)
-# ax = plt.gca()
-# ax.add_patch(Circle11)
-# ax.add_patch(Circle21)
-# ax.add_patch(Circle31)
-# ax.set_aspect('equal', adjustable='datalim')
-# plt.show()
-
-# Circle12 = plt.Circle((GL[0], V2[0]), 0.01, color='k', fill=True)
-# Circle22 = plt.Circle((GL[1], V2[1]), 0.01, color='k', fill=True)
-# Circle32 = plt.Circle((GL[2], V2[2]), 0.01, color='k', fill=True)
-# plt.figure(7)
-# plt.plot(GL, V2, linewidth= 2)
-# plt.grid()
-# plt.xlabel(r'Grau de Liberdade', fontsize= 20)
-# plt.ylabel(r'Auvetor 2', fontsize= 20)
-# ax = plt.gca()
-# ax.add_patch(Circle12)
-# ax.add_patch(Circle22)
-# ax.add_patch(Circle32)
-# ax.set_aspect('equal', adjustable='datalim')
-# plt.show()
-
-# Circle13 = plt.Circle((GL[0], V3[0]), 0.01, color='k', fill=True)
-# Circle23 = plt.Circle((GL[1], V3[1]), 0.01, color='k', fill=True)
-# Circle33 = plt.Circle((GL[2], V3[2]), 0.01, color='k', fill=True)
-# plt.figure(7)
-# plt.plot(GL, V3, linewidth= 2)
-# plt.grid()
-# plt.xlabel(r'Grau de Liberdade', fontsize= 20)
-# plt.ylabel(r'Auvetor 3', fontsize= 20)
-# ax = plt.gca()
-# ax.add_patch(Circle13)
-# ax.add_patch(Circle23)
-# ax.add_patch(Circle33)
-# ax.set_aspect('equal', adjustable='datalim')
 # plt.show()
 
 # # Frequency response for different damping ratios
